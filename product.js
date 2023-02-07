d07f8bebe762a77c575729ca8c59f651db26cf07
amy93514 <amy93514@yahoo.com.tw>
2023-02-07 21:15:18 +0800

    初版

---
+++ product.js
@@ -0,0 +1,121 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+const url = 'https://vue3-course-api.hexschool.io/v2'; 
+const path = 'mjweek2';
+
+let productModal = null;
+let delProductModal = null;
+
+const app={
+  //資料
+    data() {
+      return {
+        products: [],
+        //單一產品資訊
+        tempProduct:{
+            imagesUrl:[],
+        },
+
+        isNew:false,
+      }
+    },
+
+    //方法
+    methods:{
+
+        //取得產品
+      getProducts(){
+        axios.get(`${url}/api/${path}/admin/products`)
+        .then((res)=>{
+          this.products=res.data.products;
+          console.log(this.products);
+        })
+        .catch((err)=>{
+          console.log(err.data);
+        })
+      },
+
+      //更新產品
+      updateProduct(){
+        //用this.isNew判斷api運行
+        let updateProductUrl=`${url}/api/${path}/admin/product`;
+        let method=`post`;
+
+        if(!this.isNew){
+            //不是新增時執行
+            updateProductUrl=`${url}/api/${path}/admin/product/${this.tempProduct.id}`;
+            method=`put`;
+        }
+
+        axios[method](updateProductUrl,{data:this.tempProduct})
+        .then((res)=>{
+            //重新取得產品列表
+            this.getProducts();
+
+            //更新完關閉model
+            productModal.hide();
+        })
+        .catch((err)=>{
+          console.log(err.data);
+        })
+      },
+
+      openModel(status,product){
+        if(status==="create"){
+            productModal.show();
+            this.isNew=true;
+            //帶入初始化資料
+            this.tempProduct={
+                imagesUrl:[],
+            }
+
+        } else if(status==="edit"){
+            productModal.show();
+            this.isNew=false;
+            //帶入要編輯的資料
+            this.tempProduct={...product}
+        } else if(status==="delete"){
+          delProductModal.show();
+          this.tempProduct={...product}//取id用
+
+
+        }
+      },
+
+      //刪除單一品項
+      deleteProduct(){
+        axios.delete(`${url}/api/${path}/admin/product/${this.tempProduct.id}`)
+        .then((res)=>{
+          this.getProducts();
+          delProductModal.hide();//openModel有打開，在此關閉
+        })
+        .catch((err)=>{
+          console.log(err.data);
+        })
+      }
+
+    },
+
+    mounted(){
+      //取出存在cookie的token
+      const token = document.cookie.replace(/(?:(?:^|.*;\s*)mjweek2\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+      //發出請求headers預設帶入token
+      axios.defaults.headers.common['Authorization'] = token;
+      this.getProducts();
+
+      //Bootstrap方法
+      console.log(bootstrap);
+
+      //先初始化new
+      productModal = new bootstrap.Modal("#productModal");
+      //再呼叫方法
+
+      //刪除的model
+      delProductModal= new bootstrap.Modal("#delProductModal");
+
+
+    }
+  };
+
+createApp(app).mount('#app');
